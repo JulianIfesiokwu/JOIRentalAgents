f1be50e0f549b0f1f59b89401d0b54f4952ea97c
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2024-02-08 17:10:03 +0100

    Feat: adding project assets, logos, font-family and colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // project default font
+      fontFamily: {
+        urbanist: ["Urbanist", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
