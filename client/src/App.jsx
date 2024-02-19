777168d890bed777225ae99cfe9b249c8fe665ff
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2024-02-19 10:50:56 +0100

    Feat: created pages and added routing to page components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,39 @@
-import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import SharedLayout from "./components/SharedLayout";
+import Home from "./pages/Home";
+import Properties from "./pages/Properties";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
 
 function App() {
-  return <Header />;
+  return (
+    <Routes>
+      <Route element={<SharedLayout />}>
+        <Route
+          path='/'
+          index
+          element={<Home />}
+        />
+        <Route
+          path='/properties'
+          element={<Properties />}
+        />
+        <Route
+          path='/about'
+          element={<About />}
+        />
+        <Route
+          path='/contact'
+          element={<Contact />}
+        />
+        <Route
+          path='/services'
+          element={<Services />}
+        />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
