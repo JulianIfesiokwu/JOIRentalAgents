54988d7776f78687712f378507a6aaf4bf5e4652
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2024-04-30 14:52:42 +0100

    Feat: adding image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,20 +1,62 @@
 import { useSelector } from "react-redux";
 import ClientDetails from "../components/ClientDetails";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Spinner } from "flowbite-react";
+import {
+  getStorage,
+  uploadBytesResumable,
+  ref,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 const Profile = () => {
   const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
+  const fileref = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
 
   const currentUser = useSelector((state) => state.user);
-  console.log(currentUser.currentUser);
 
   const handleChange = (e) => {
     if (e.target.id === "email" || e.target.id === "password") {
       setFormData({ ...formData, [e.target.id]: e.target.value });
     }
   };
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+
+      (error) => {
+        setFileUploadError(true);
+      },
+
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, profilePicture: downloadURL });
+        });
+      }
+    );
+  };
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
 
   return (
     <div className='bg-[#141414]'>
@@ -23,11 +65,37 @@ const Profile = () => {
           {currentUser.currentUser.username} Profile
         </h1>
         <form className='flex flex-col gap-4 w-2/4'>
+          <input
+            type='file'
+            className=''
+            ref={fileref}
+            hidden
+            accept='image/*'
+            onChange={(e) => setFile(e.target.files[0])}
+          />
           <img
-            src={currentUser.profilePicture}
+            src={formData.profilePicture || currentUser.profilePicture}
             alt={currentUser.username}
             className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+            onClick={() => {
+              fileref.current.click();
+            }}
           />
+          <p className='text-center my-4 text-sm'>
+            {fileUploadError ? (
+              <span className='text-red-700'>
+                Error uploading image (Image must be less than 2mb)
+              </span>
+            ) : filePerc > 0 && filePerc < 100 ? (
+              <span className='text-green-700'>{`Uploading ${filePerc}`}</span>
+            ) : filePerc === 100 ? (
+              <span className='text-green-700'>
+                Image uploaded successfully
+              </span>
+            ) : (
+              ""
+            )}
+          </p>
           <ClientDetails
             type={"username"}
             placeholder={"Your username"}
